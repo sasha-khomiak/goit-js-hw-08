8bc56149b84f84c6e778b6763ecdf1b05a729b39
Oleksandr Khomiak <khomiak@gmail.com>
2023-03-14 19:23:29 +0200

    2 задача

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+// отут "videoplayer-current-time" можна винести в константу
+// як казав репета, наприклад CURRENT_TIME
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function(ev) {
+    // console.log(ev);
+    localStorage.setItem("videoplayer-current-time", ev.seconds)
+    
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
